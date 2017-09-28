3937039ecf4bd93918da499680adbd48e6a94395
Bill <beiller@gmail.com>
2017-09-28 02:41:38 -0400

    OK

--- build_script_custom.sh
+++ build_script_custom.sh
@@ -1,7 +1,7 @@
 #CC="gcc -DHAVE_CONFIG_H -I.  -pthread    -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16  -Ofast -flto -fuse-linker-plugin -funroll-loops -fvariable-expansion-in-unroller -ftree-loop-if-convert-stores -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -maes  -march=native -O2"
 #CC2="gcc -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16  -Ofast -flto -fuse-linker-plugin -funroll-loops -fvariable-expansion-in-unroller -ftree-loop-if-convert-stores -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -maes  -march=native -O2"
-CC="clang -maes -O4 -Ofast -march=native -fomit-frame-pointer -funroll-loops -ffast-math"
-CC2="clang -maes -O4 -Ofast -march=native -fomit-frame-pointer -funroll-loops -ffast-math"
+CC="clang -mno-aes -mno-avx -mno-avx2 -O4 -Ofast -march=native -fomit-frame-pointer -funroll-loops -ffast-math"
+CC2="clang -mno-aes -mno-avx -mno-avx2 -O4 -Ofast -march=native -fomit-frame-pointer -funroll-loops -ffast-math"
 
 $CC -c -o minerd-cpu-miner.o cpu-miner.c
 
@@ -19,6 +19,12 @@ $CC -c -o crypto/minerd-c_jh.o crypto/c_jh.c
 
 $CC -c -o crypto/minerd-c_skein.o crypto/c_skein.c
 
+$CC -c -o minerd-oaes_lib.o oaes_lib.c
+
+$CC -c -o crypto/minerd-aesb.o crypto/aesb.c
+
+#$CC -c -o crypto/minerd-hash.o crypto/hash.c
+
 $CC -c -o minerd-cryptonight.o cryptonight_aesni.c
 #$CC -c -o minerd-cryptonight.o cryptonight.c
 
@@ -31,6 +37,8 @@ crypto/minerd-c_groestl.o \
 crypto/minerd-c_blake256.o \
 crypto/minerd-c_jh.o \
 crypto/minerd-c_skein.o \
+crypto/minerd-aesb.o \
+minerd-oaes_lib.o \
 minerd-cryptonight.o -L/usr/lib/x86_64-linux-gnu -lcurl -ljansson -lpthread
 
 
